e846c6d2deca136073a832d2d98df045debb99a3
Habibi  Yusrizal <habibi.yusrizal@Habibis-MacBook-Pro.local>
2025-01-12 03:55:03 +0800

    Work Content: - Giving path to the about page and home page - Add 3 action in articles listing pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
 
-  root "pages#index"
+  root "pages#home"
   get "/about", to: "pages#about"
   resources :articles, only: [ :show, :index, :new, :create, :edit, :update, :destroy ]
 end
