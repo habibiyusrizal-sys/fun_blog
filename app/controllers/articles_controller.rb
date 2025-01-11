688088979fe51c2e332368d5e13f9eea6eec2bb1
Habibi  Yusrizal <habibi.yusrizal@Habibis-MacBook-Pro.local>
2025-01-11 14:14:01 +0800

    - Add edit feature - Add update feature - Add delete feature - Add validation and flash messages - Create partial _form - Refactoring code for @article = params.find([:id]) in before_action way

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,51 @@
 class ArticlesController < ApplicationController
-  def show
-    @article = Article.find(params[:id])
+  before_action :set_article, only: %i[show edit update destroy]
+  def index
+    @articles = Article.all
   end
+  def show
+    # @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    # @article = Article.find(params[:id])
+  end
+
+  def update
+    # @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 end
